6b88a8cd2cf29a359ae3e788c58bba14c3bf04bc
Gaboanchique <gaboanchique@gmail.com>
2023-07-10 21:31:21 -0500

    proyecto creado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-nuevo-proyecto';
+  title = 'si arranco mi primer proyecto en Angular';
 }
