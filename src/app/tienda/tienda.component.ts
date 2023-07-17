63aa66b837f392e0e478d3fcac0499b3637fb2a0
Gaboanchique <gaboanchique@gmail.com>
2023-07-17 09:12:37 -0500

    angular.json-> clic, se añanade boostrap en componente de inicio

--- src/app/tienda/tienda.component.ts
+++ src/app/tienda/tienda.component.ts
@@ -6,12 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tienda.component.css']
 })
 export class TiendaComponent implements OnInit {
-ruta = "inicio";
+
+  ruta ="inicio";
+
   constructor() { }
 
   ngOnInit(): void {
   }
-  escogerPagina(rutaHijo:string) {
-    this.ruta = rutaHijo;
+
+  escogerPaginax(rutaDersdeHijo:string){
+    console.log("aca");
+    this.ruta = rutaDersdeHijo;
   }
+
 }
